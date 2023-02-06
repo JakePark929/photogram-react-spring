bf60fda6619940e3309e935b5c03d5ec63b0ea46
epike <epikeran@gmail.com>
2023-02-06 16:54:54 +0900

    사진 업로드 관련 페이지 수정

--- reactfront/src/pages/user/ProfilePage.js
+++ reactfront/src/pages/user/ProfilePage.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './Profile.css'
-import {useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import {faCog, faHeart} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Modal} from "react-bootstrap";
@@ -9,20 +9,36 @@ import {useSelector} from "react-redux";
 const ProfilePage = (props) => {
     const {ip, port, storeIp, storePort} = useSelector((store) => store);
     const navigate = useNavigate();
-    const id = props.id
-    const [user, setUser] = useState({
-        id: "",
-        username: "",
-        name: "",
-        email: "",
-        phone: "",
-        gender: "",
-        website: "",
-        bio: "",
-        profileImageUrl: "",
-        privateFileUrl: "",
-        images: ""
+    const url = useLocation().pathname;
+    let pageNumber = url.substring(url.lastIndexOf("/") + 1);
+    const principal = props.principal;
+    const [data, setData] = useState({
+        pageOwner: "",
+        imageCount: "",
+        user: {
+            images: "",
+
+        },
     });
+    // const [user, setUser] = useState({
+    //     id: "",
+    //     username: "",
+    //     name: "",
+    //     email: "",
+    //     phone: "",
+    //     gender: "",
+    //     website: "",
+    //     bio: "",
+    //     profileImageUrl: "",
+    //     privateFileUrl: "",
+    //     images: {
+    //         id: "",
+    //         caption: "",
+    //         postImageUrl: "",
+    //         createDate: "",
+    //     },
+    //     isPageOwner: ""
+    // });
     const [image, setImage] = useState({
         id: "",
         caption: "",
@@ -33,10 +49,14 @@ const ProfilePage = (props) => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const showData = () => {
+        console.log(data);
+        console.log(data.user.images);
+    }
 
     useEffect(() => {
-        fetch(ip + port + "/api/user/" + id).then(res => res.json()).then(res => {
-            setUser(res.data);
+        fetch(ip + port + "/api/user/" + pageNumber).then(res => res.json()).then(res => {
+            setData(res.data);
         });
     }, []);
 
@@ -72,10 +92,14 @@ const ProfilePage = (props) => {
 
                     <div className="profile-right">
                         <div className="name-group">
-                            <h2>{user.username}</h2>
+                            <h2>{data.user.username}</h2>
 
-                            <button className="cta" onClick={() => navigate("/image/upload")}>사진등록</button>
-                            <button className="cta">구독하기</button>
+                            {
+                                data.pageOwner ?
+                                    <button className="cta" onClick={() => navigate("/image/upload")}>사진등록</button>
+                                    : <button className="cta">구독하기</button>
+                            }
+                            {/*<button className="cta" onClick={showData}>유저정보</button>*/}
                             <button className="modi" onClick={handleShow}>
                                 <FontAwesomeIcon icon={faCog}/>
                             </button>
@@ -83,15 +107,15 @@ const ProfilePage = (props) => {
 
                         <div className="subscribe">
                             <ul>
-                                <li><a href=""> 게시물<span>3</span>
+                                <li><a href=""> 게시물<span>{data.imageCount}</span>
                                 </a></li>
                                 <li><a href=""> 구독정보<span>2</span>
                                 </a></li>
                             </ul>
                         </div>
                         <div className="state">
-                            <h4>{user.bio}</h4>
-                            <h4>{user.website}</h4>
+                            <h4>{data.user.bio}</h4>
+                            <h4>{data.user.website}</h4>
                         </div>
                     </div>
 
@@ -103,8 +127,8 @@ const ProfilePage = (props) => {
                     <div id="tab-1-content" className="tab-content-item show">
                         <div className="tab-1-content-inner">
                             {
-                                user.images === '' ? '' :
-                                    user.images.map((image) =>
+                                data.user.images === '' ? '' :
+                                    data.user.images.map((image) =>
                                         <div className="img-box">
                                             <a href="">
                                                 <img
@@ -128,7 +152,7 @@ const ProfilePage = (props) => {
             {/*모달*/}
             <Modal className="modal-info" size="sm" show={show} onHide={handleClose}>
                 <Modal.Body className="modal">
-                    <button onClick={() => navigate("/user/" + id + "/update")}>
+                    <button onClick={() => navigate("/user/" + principal.id + "/update")}>
                         회원정보변경
                     </button>
                     <button onClick={logout}>
